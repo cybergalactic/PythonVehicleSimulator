6a87419064f39332aa8ef63220abcac94e0143de
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-16 15:11:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 15:07:02 2021
+
+@author: thor
+"""
+
+class ClassName:
+    def __init__(self, *args, **kwargs):
+        pass
+    
