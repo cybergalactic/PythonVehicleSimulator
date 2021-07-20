72c0945ebd5651130df3c210678384f1b4c194f5
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-20 18:32:43 +0200

    Update guidance.py

--- functions/guidance.py
+++ functions/guidance.py
@@ -15,17 +15,23 @@ Revisions:
 import numpy as np
 import math
 
-# refModel3(x_d, v_d, r_f, r, wn, zeta, sampleTime) is a 3-order reference 
-# model for generation of a smooth desired position x_d, velocity v_d, and
-# acceleration a_d. Inputs are natural frequency wn and relative damping zeta.
-def refModel3(x_d, v_d, r_f, r, wn, zeta, sampleTime):
+# refModel3(x_d, v_d, a_d, r, wn_d, zeta_d, v_max, sampleTime) is a 3-order reference 
+# model for generation of a smooth desired position x_d, velocity |v_d| < v_max , and
+# acceleration a_d. Inputs are natural frequency wn_d and relative damping zeta_d.
+def refModel3(x_d, v_d, a_d, r, wn_d, zeta_d, v_max, sampleTime):
+
+    # velocity saturation
+    if (v_d > v_max):
+        v_d = v_max
+    elif (v_d < -v_max): 
+        v_d = -v_max
     
-    # desired acceleration
-    a_d = wn**2 *( r_f - x_d ) - 2 * zeta * wn * v_d
-    
+    # desired "jerk"
+    j_d = wn_d**3 * (r - x_d) - (2*zeta_d+1) * wn_d**2 * v_d - (2*zeta_d+1) * wn_d * a_d
+
    # forward Euler integration
     x_d = x_d + sampleTime * v_d                # desired position
     v_d = v_d + sampleTime * a_d                # desired velocity
-    r_f = r_f + sampleTime * wn * (r - r_f)     # LP-filtered setpoint r
+    a_d = a_d + sampleTime * j_d                # desired acceleration 
     
     return x_d, v_d, a_d
