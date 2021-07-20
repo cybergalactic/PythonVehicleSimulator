09621c5eb970e6f61b341f94f52624a7d7563c7d
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-21 00:11:28 +0200

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .kinematics import Rzyx, Tzyx, attitudeEuler
+from .kinematics import Rmtrx, Hmtrx,Rzyx, Tzyx, attitudeEuler
 from .mainLoop import simulate,simInfo
 from .plotTimeSeries import plotVehicleStates, plotControls
 from .guidance import refModel3
