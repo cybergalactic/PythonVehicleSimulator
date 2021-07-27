394725b766ac796bedbb0bd2dd8ec68260ca34af
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-27 17:36:27 +0200

    Update control.py

--- functions/control.py
+++ functions/control.py
@@ -20,19 +20,18 @@ from functions.guidance import refModel3
 
 #  u = PIDpolePlacement(_x,e_v,e_int,x_d,v_d,a_d,m,d,k,wn_d,zeta_d,wn,zeta,r,v_max,sampleTime)
 def PIDpolePlacement(e_x,e_v,e_int,x_d,v_d,a_d,m,d,k,wn_d,zeta_d,wn,zeta,r,v_max,sampleTime):
-
-        # PID gains based on pole placement
-        Kp = m * wn**2 - k
-        Kd = m * 2 * zeta * wn - d
-        Ki = (wn/10) * Kp
+    # PID gains based on pole placement
+    Kp = m * wn**2 - k
+    Kd = m * 2 * zeta * wn - d
+    Ki = (wn/10) * Kp
         
-        # PID control law
-        u = -Kp * e_x - Kd * e_v - Ki * e_int
+    # PID control law
+    u = -Kp * e_x - Kd * e_v - Ki * e_int
 
-        # Forward Euler integration
-        e_int = e_int + sampleTime * e_x
+    # Forward Euler integration
+    e_int = e_int + sampleTime * e_x
         
-        # 3rd-order reference model for smooth position, velocity and acceleration
-        [x_d, v_d, a_d] = refModel3(x_d, v_d, a_d, r, wn_d, zeta_d, v_max, sampleTime)
+    # 3rd-order reference model for smooth position, velocity and acceleration
+    [x_d, v_d, a_d] = refModel3(x_d, v_d, a_d, r, wn_d, zeta_d, v_max, sampleTime)
 
-        return u, e_int, x_d, v_d, a_d    
+    return u, e_int, x_d, v_d, a_d    
