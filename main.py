dffc50f6b3481c8d7b4f1b295408836f87185202
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-27 09:06:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 main.py: Main program for the Python Vehicle Simulator, which can be used
@@ -9,31 +8,33 @@ Motion Control. 2nd. Edition, Wiley.
 URL: www.fossen.biz/wiley    
     
 Author:     Thor I. Fossen
-Date:       18 July 2021
+Date:       25 July 2021
 """
 import matplotlib.pyplot as plt
 from functions import plotVehicleStates,plotControls,simulate
 from vehicles import DSRV,otter
 
-#  DSRV('stepInput',15.0)          Stern plan step input
-#  DSRV('depthAutopilot',30.0)     PID depth autopilot
-vehicle1 = DSRV('depthAutopilot',60.0) 
-vehicle2 = DSRV('stepInput',25.0) 
+#  DSRV()                           Stern plan step input
+#  DSRV('depthAutopilot',30.0)      PID depth autopilot
+#vehicle1 = DSRV('depthAutopilot',60.0) 
+vehicle1 = DSRV('depthAutopilot',30.0) 
+#vehicle2 = otter() 
+vehicle2 = otter('headingAutopilot',100.0,0,0.1,120) 
 
 # Simulation parameters: sample time and number of samples
-sampleTime = 0.1
-N = 1000            
+sampleTime = 0.02
+N = 2500            
 
 # Main simulation loop 
 def main():
     
-    [simTime1, simData1] = simulate(N, sampleTime, vehicle1)   
-    plotVehicleStates(simTime1, simData1, 1)                    
-    plotControls(simTime1, simData1, vehicle1, 2)
+    #[simTime1, simData1] = simulate(N, sampleTime, vehicle1)   
+    #plotVehicleStates(simTime1, simData1, 1)                    
+    #plotControls(simTime1, simData1, vehicle1, 2)
 
     [simTime2, simData2] = simulate(N, sampleTime, vehicle2)   
-    #plotVehicleStates(simTime2, simData2, 3)    
-    #plotControls(simTime2, simData2, vehicle2, 4)
+    plotVehicleStates(simTime2, simData2, 3)    
+    plotControls(simTime2, simData2, vehicle2, 4)
 
     plt.show()
 
