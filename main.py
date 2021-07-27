8a9e4f2b2340132d4f48868d26f55eb00539abe3
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-27 22:13:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,37 +14,34 @@ import matplotlib.pyplot as plt
 from functions import plotVehicleStates,plotControls,simulate
 from vehicles import DSRV,otter,shipClarke83
 
-""" 
-DSRV()                                    Stern plan, step input
-DSRV('depthAutopilot',z_d)                PID depth autopilot
-
-otter()                                   Propeller revolutions, step inputs
-otter('headingAutopilot',psi_d,V_c,beta_c,tau_X)   PID heading autopilot
-
-shipClarke83()                            Rudder, step input
-shipClarke83('headingAutopilot',L,B,T,psi_d,L,B,T,Cb,V_current,beta_c,tau_X)
-   PID heading autopilot
-"""
-
-# vehicle1 = DSRV('depthAutopilot',60.0) 
-vehicle1 = shipClarke83() 
-
-vehicle2 = otter('headingAutopilot',100.0,0,0.1,120) 
-
 # Simulation parameters: sample time and number of samples
 sampleTime = 0.02
-N = 3000            
+N = 5000 
+
+""" 
+DSRV()                                             Stern plan, step input
+otter()                                            Propeller revolutions, step inputs
+shipClarke83()                                     Rudder angle, step input
+
+DSRV('depthAutopilot',z_d)                         PID depth autopilot
+otter('headingAutopilot',psi_d,V_c,beta_c,tau_X)   PID heading autopilot
+shipClarke83('headingAutopilot',psi_d,L,B,T,Cb,V_current,beta_c,tau_X) PID heading autopilot
+"""
+
+vehicle1 = DSRV('depthAutopilot',60.0) 
+vehicle2 = shipClarke83('headingAutopilot',-20,70,8,6,0.7,0.5,-10,1e5)
+vehicle3 = otter('headingAutopilot',100.0,0,0.1,120) 
 
 # Main simulation loop 
 def main():
     
-    [simTime1, simData1] = simulate(N, sampleTime, vehicle1)   
-    plotVehicleStates(simTime1, simData1, 1)                    
-    plotControls(simTime1, simData1, vehicle1, 2)
+    #[simTime1, simData1] = simulate(N, sampleTime, vehicle1)   
+    #plotVehicleStates(simTime1, simData1, 1)                    
+    #plotControls(simTime1, simData1, vehicle1, 2)
 
-    #[simTime2, simData2] = simulate(N, sampleTime, vehicle2)   
-    #plotVehicleStates(simTime2, simData2, 3)    
-    #plotControls(simTime2, simData2, vehicle2, 4)
+    [simTime2, simData2] = simulate(N, sampleTime, vehicle2)   
+    plotVehicleStates(simTime2, simData2, 3)    
+    plotControls(simTime2, simData2, vehicle2, 4)
 
     plt.show()
 
