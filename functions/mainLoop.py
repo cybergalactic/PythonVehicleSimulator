cb747d00288965880876dcd19992d199374357bd
Thor I. Fossen <thor.fossen@ntnu.no>
2021-07-28 09:33:36 +0200

    Update mainLoop.py

--- functions/mainLoop.py
+++ functions/mainLoop.py
@@ -22,21 +22,22 @@ def simulate(N, sampleTime, vehicle):
     DOF = 6                     # degrees of freedom
     t = 0                       # initial simulation time
 
-    # initial state vectors
-    eta = np.array( [0, 0, 0, 0, 0,0], float)    # user editable
-    nu = vehicle.nu                              # defined by vehicle class   
+    # Initial state vectors
+    eta = np.array( [0, 0, 0, 0, 0,0], float)    # position/attitude, user editable
+    nu = vehicle.nu                              # velocity, defined by vehicle class
+    u_actual = vehicle.u_actual                  # actual inputs, defined by vehicle class  
 
     simInfo(vehicle)            # print simulation info
 
-    # initialization of table used to store the simulation data
-    simData = np.empty( [0, 2*DOF + vehicle.dimU], float)
+    # Initialization of table used to store the simulation data
+    simData = np.empty( [0, 2*DOF + 2 * vehicle.dimU], float)
     
-    # simulator for-loop
+    # Simulator for-loop
     for i in range(0,N+1):
         
         t = i * sampleTime      # simulation time
         
-        # vehicle specific control systems
+        # Vehicle specific control systems
         if (vehicle.controlMode == 'depthAutopilot'):
             u_control = vehicle.depthAutopilot(eta,nu,sampleTime)
         elif (vehicle.controlMode == 'headingAutopilot'):
@@ -44,14 +45,15 @@ def simulate(N, sampleTime, vehicle):
         elif (vehicle.controlMode == 'stepInput'):
             u_control = vehicle.stepInput(t)          
         
-        # store simulation data in simData
-        simData = np.vstack( [simData, np.append(np.append(eta,nu),u_control)] )
-        
+        # Store simulation data in simData
+        signals = np.append( np.append( np.append(eta,nu),u_control), u_actual )
+        simData = np.vstack( [simData, signals] ) 
+
         # Propagate vehicle and attitude dynamics
-        nu  = vehicle.dynamics(eta,nu,u_control,sampleTime)
+        [nu, u_actual]  = vehicle.dynamics(eta,nu,u_actual,u_control,sampleTime)
         eta = attitudeEuler(eta,nu,sampleTime)
 
-    # store simulation time vector
+    # Store simulation time vector
     simTime = np.arange(start=0, stop=t+sampleTime, step=sampleTime)[:, None]
 
     return(simTime,simData)
