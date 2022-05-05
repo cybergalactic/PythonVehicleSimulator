47e0b14f12fea8ec2a1f75b16d916f250089c226
Thor I. Fossen <thor.fossen@ntnu.no>
2022-05-05 13:42:09 +0200

    Update mainLoop.py

--- src/python_vehicle_simulator/mainLoop.py
+++ src/python_vehicle_simulator/mainLoop.py
@@ -1,21 +1,62 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Main simulation loop.
+Main simulation loop called by main.py.
 
 Author:     Thor I. Fossen
 """
 
-from .gnc import attitudeEuler
 import numpy as np
+from .gnc import attitudeEuler
 
-# Function simInfo(vehicle)
-def simInfo(vehicle):
-    print('\nThe Python Vehicle Simulator:')
-    print('Vehicle:         %s, L = %s' % (vehicle.name, vehicle.L))
-    print('Control system:  %s' % (vehicle.controlDescription))
-   
-        
+###############################################################################
+# Function printSimInfo(vehicle)
+###############################################################################
+def printSimInfo():
+    
+    print('---------------------------------------------------------------------------------------')
+    print('The Python Vehicle Simulator')
+    print('---------------------------------------------------------------------------------------')
+    print('1 - Deep submergence rescue vehicle (DSRV): controlled by a stern plane, L = 5.0 m')
+    print('2 - Frigate: rudder-controlled ship described by a nonlinear Nomoto model, L = 100.0 m')
+    print('3 - Otter unmanned surface vehicle (USV): controlled by two propellers, L = 2.0 m')
+    print('4 - ROV Zefakkel: rudder-controlled ship described by a nonlinear Nomoto model, L = 54.0 m')
+    print('5 - Semisubmersible: controlled by tunnel thrusters and main propellers, L = 84.5 m')
+    print('6 - Ship: linear maneuvering model specified by L, B and T using the Clarke (1983) formulas')
+    print('7 - Offshore supply vessel: controlled by tunnel thrusters and main propellers, L = 76.2 m')
+    print('8 - Tanker: rudder-controlled ship model including shallow water effects, L = 304.8 m')
+    # print('9 - Remus100: AUV controlled by stern planes and a propeller, L = xx m')
+    print('---------------------------------------------------------------------------------------')
+
+    """
+    Constructors used to define the vehicle objects as cases in main.py:
+        vehicle = DSRV('depthAutopilot',60.0) 
+        vehicle = frigate('headingAutopilot',10.0,100.0)    
+        vehicle = otter('headingAutopilot',100.0,0.3,-30.0,200.0) 
+        vehicle = ROVzefakkel('headingAutopilot',3.0,100.0)
+        vehicle = semisub('DPcontrol',10.0,2.0,20.0,0.5,-20.0)
+        vehicle = shipClarke83('headingAutopilot',-20.0,70,8,6,0.7,0.5,-10.0,1e5)
+        vehicle = supply('DPcontrol',4.0,4.0,100.0,0.5,-20.0)
+        vehicle = tanker('headingAutopilot',-20,0.5,150,20,80)
+    """   
+    
+    
+###############################################################################    
+# Function printVehicleinfo(vehicle)
+###############################################################################
+def printVehicleinfo(vehicle, sampleTime, N): 
+    print('---------------------------------------------------------------------------------------')
+    print('%s' % (vehicle.name))
+    print('Length: %s m' % (vehicle.L))
+    print('%s' % (vehicle.controlDescription))  
+    print('Sampling frequency: %s Hz' % round(1 / sampleTime))
+    print('Simulation time: %s seconds' % round(N * sampleTime))
+    print('---------------------------------------------------------------------------------------')
+    
+
+###############################################################################
 # Function simulate(N, sampleTime, vehicle)
+###############################################################################
 def simulate(N, sampleTime, vehicle):
     
     DOF = 6                     # degrees of freedom
@@ -24,10 +65,8 @@ def simulate(N, sampleTime, vehicle):
     # Initial state vectors
     eta = np.array([0, 0, 0, 0, 0, 0], float)    # position/attitude, user editable
     nu = vehicle.nu                              # velocity, defined by vehicle class
-    u_actual = vehicle.u_actual                  # actual inputs, defined by vehicle class  
-
-    simInfo(vehicle)            # print simulation info
-
+    u_actual = vehicle.u_actual                  # actual inputs, defined by vehicle class
+    
     # Initialization of table used to store the simulation data
     simData = np.empty( [0, 2*DOF + 2 * vehicle.dimU], float)
 
