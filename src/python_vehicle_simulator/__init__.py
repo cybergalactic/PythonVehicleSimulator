26850653b41331d21218c51f3c1aca73c64eb8d2
Thor I. Fossen <thor.fossen@ntnu.no>
2022-05-08 09:16:29 +0200

    Delete __init__.py

--- src/python_vehicle_simulator/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .gnc import *
-from .mainLoop import *
-from .plotTimeSeries import *
-from .guidance import *
-from .control import *
-from .models import *
