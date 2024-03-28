36952888dc6a48a21ee70e720b83491fb9b84ec9
Thor I. Fossen <thor.fossen@ntnu.no>
2024-03-28 09:37:52 +0100

    Update supply.py

--- src/python_vehicle_simulator/vehicles/supply.py
+++ src/python_vehicle_simulator/vehicles/supply.py
@@ -6,7 +6,7 @@ supply.py:
     The constructors are:
 
     supply()                                      
-        Step inputs for propeller revolutions n1, n2, n3 and n4
+        Step inputs for the propeller speeds n1, n2, n3, n4, n5 and n6
     supply('DPcontrol',x_d,y_d,psi_d,V_c,beta_c)  DP control system
         x_d: desired x position (m)
         y_d: desired y position (m)
@@ -20,11 +20,13 @@ Methods:
         returns nu[k+1] and u_actual[k+1] using Euler's method. 
         The control inputs are:
 
-            u_control = n  (RPM)
-            n = [ #1 Bow thruster (RPM) 
-                 #2 Bow thruster (RPM)
-                 #3 Right main propeller (RPM)
-                 #4 Left main propeller (RPM) ]
+        u_control = n 
+        n = [  #1 Bow tunnel thruster (RPM),
+               #2 Bow tunnel thruster (RPM),
+               #3 Stern tunnel thruster (RPM),
+               #4 Stern tunnel thruster (RPM),
+               #5 Right main propeller (RPM),
+               #6 Left main propeller (RPM)]
 
     u_alloc = controlAllocation(tau)
         Control allocation based on the pseudoinverse                 
@@ -89,7 +91,7 @@ class supply:
             )
 
         else:
-            self.controlDescription = "Step inputs for n = [n1, n2, n3, n4]"
+            self.controlDescription = "Step inputs n = [n1, n2, n3, n4, n5, n6]"
             controlSystem = "stepInput"
 
         self.ref = np.array([r_x, r_y, r_n * D2R], float)
@@ -99,27 +101,35 @@ class supply:
 
         # Initialize the supply vessel model
         m = 6000.0e3        # mass (kg)
-        self.L = 76.2       # Length (m)
+        self.L = 76.2       # length (m)
         self.T_n = 1.0      # prop. rev. time constant (s)
-        self.n_max = np.array([250, 250, 160, 160], float)  # RPM saturation limits (N)
-        self.nu = np.array([0, 0, 0, 0, 0, 0], float)  # velocity vector
-        self.u_actual = np.array([0, 0, 0, 0], float)  # RPM inputs
+        self.n_max = np.array([250, 250, 150, 250, 
+                               160, 160], float) # RPM saturation limits
+        self.nu = np.array([0, 0, 0, 0, 0, 0], float) # initial velocity vector
+        self.u_actual = np.array([0, 0, 0, 0, 0, 0], float) # RPM inputs
         self.name = "Offshore supply vessel (see 'supply.py' for more details)"
 
         # Two tunnel thrusters in the bow, no. 1 and 2
+        # Two tunnel thrusters in the stern, no. 3 and 4
         # Two main propellers aft, no. 3 and 4
         self.controls = [
-            "#1 Bow thruster (RPM)",
-            "#2 Bow thruster (RPM)",
-            "#3 Right main propeller (RPM)",
-            "#4 Left main propeller (RPM)",
+            "#1 Bow tunnel thruster (RPM)",
+            "#2 Bow tunnel thruster (RPM)",
+            "#3 Stern tunnel thruster (RPM)",
+            "#4 Stern tunnel thruster (RPM)",
+            "#5 Right main propeller (RPM)",
+            "#6 Left main propeller (RPM)"
         ]
         self.dimU = len(self.controls)
 
         # Thrust coefficient and configuration matrices (Fossen 2021, Ch. 11.2)
-        K = np.diag([2.4, 2.4, 17.6, 17.6])
+        # Thrust_max(i) = K(i) * n_max(i)^2
+        # Tunnel thruster: 3.2 * 250^2 = 200 kN
+        # Main propeller: 31.2 * 160^2 = 799 kN
+        K = np.diag([3.2, 3.2, 3.2, 3.2, 31.2, 31.2])
         T = np.array(
-            [[0, 0, 1, 1], [1, 1, 0, 0], [30, 22, -self.L / 2, self.L / 2]], float
+            [ [0, 0, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0], 
+              [30, 22, -22, -30, -8, 8] ], float
         )
         self.B = T @ K
 
@@ -144,7 +154,7 @@ class supply:
         self.x_d = 0.0  # setpoints
         self.y_d = 0.0
         self.psi_d = 0.0
-        self.wn = np.diag([0.3, 0.3, 0.1])  # PID pole placement
+        self.wn = np.diag([0.1, 0.1, 0.1])    # PID pole placement
         self.zeta = np.diag([1.0, 1.0, 1.0])
 
 
@@ -158,10 +168,10 @@ class supply:
         n = u_actual
 
         # Current velocities
-        u_c = self.V_c * math.cos(self.beta_c - eta[5])  # current surge velocity
-        v_c = self.V_c * math.sin(self.beta_c - eta[5])  # current sway velocity
+        u_c = self.V_c * math.cos(self.beta_c - eta[5]) # current surge velocity
+        v_c = self.V_c * math.sin(self.beta_c - eta[5]) # current sway velocity
 
-        nu_c = np.array([u_c, v_c, 0, 0, 0, 0], float)  # current velocity vector
+        nu_c = np.array([u_c, v_c, 0, 0, 0, 0], float) # current velocity vector
         nu_r = nu - nu_c  # relative velocity vector
 
         # Control forces and moments with propeller saturation
@@ -243,12 +253,12 @@ class supply:
         """
         u = stepInput(t) generates propeller step inputs (RPM).
         """
-        n = np.array([0, 0, 100, 100], float)
+        n = np.array([0, 0, 0, 0, 100, 100], float)
 
         if t > 30:
-            n = np.array([50, 50, 50, 50], float)
+            n = np.array([50, 50, 50, 50, 50, 50], float)
         if t > 70:
-            n = np.array([0, 0, 0, 0], float)
+            n = np.array([0, 0, 0, 0, 0, 0], float)
 
         u_control = n
 
